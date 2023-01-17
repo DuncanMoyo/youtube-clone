4bfa032114d8f80167ddca185655f9ad32b8298e
Duncan Moyo <duncan@darkpools.co.za>
2023-01-17 14:06:58 +0200

    Videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import {VideoCard, ChannelCard} from './'
 
 const Videos = ({ videos }) => {
   // console.log("🚀 ~ file: Videos.jsx:4 ~ Videos ~ Videos", videos);
 
-  return <div>Videos</div>;
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.channelId && <ChannelCard channelDetail={item} /> }
+          {item.id.videoId && <VideoCard video={item} /> }
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
